f0930776eea4f87f4159e1064d624336bf82b7d6
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2024-04-04 18:47:48 +0800

    Removed Dependency Folders

--- authority_daemon/src/participant.py
+++
@@ -1,6 +0,0 @@
-class Participant:
-    def __init__(self, eth_address: str):
-        self.eth_address = eth_address
-    
-    def prime(self):
-        return
--- authority_daemon/src/private2public.ts
+++
@@ -1,8 +0,0 @@
-import EthCrypto from 'eth-crypto'
-
-process.stdin.on("data", data => {
-    const private_key = data.toString().trim();
-    console.log(EthCrypto.publicKeyByPrivateKey(private_key))
-    process.exit()
-}
-)
