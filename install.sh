62841b19ff4eda62e71322374cf125635e0dd12b
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2024-04-06 20:48:14 +0800

    Adjusted Installer to include Creation of Necessary Data Subdirectories for the authority daemon and the Isolator

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+mkdir isolator/data/elections
+mkdir authority_daemon/data/control_keys
+mkdir authority_daemon/data/dates
+mkdir authority_daemon/data/roles
 sudo cp default /etc/nginx/sites-enabled
 sudo systemctl restart nginx
 python3 -m venv authority_daemon
